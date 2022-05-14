992a0950868159bad31e4762e75d0845dae4d50d
devnello <marcello.spedicato@gmail.com>
2022-05-14 10:29:42 +0200

    Change Login

--- resources/js/Shared/UserMenu.vue
+++ resources/js/Shared/UserMenu.vue
@@ -22,6 +22,13 @@
         <div :class="isUrl('contacts') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Contacts</div>
       </Link>
     </div>
+    <div class="mb-4">
+      <Link class="group flex items-center py-3" href="/login">
+        <icon name="printer" class="mr-2 w-4 h-4"
+              :class="isUrl('documentos') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'"/>
+        <div :class="isUrl('documentos') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Login</div>
+      </Link>
+    </div>
   </div>
 </template>
 
